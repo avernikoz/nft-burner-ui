f27672f3c2aeeed42acb9af5541af5f81ed5be74
avernikoz <avernikoz@gmail.com>
2023-09-19 22:11:38 +0300

    add webgl framework & initial set up

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { main } from "../../webl/main";
 
 function App() {
+  useEffect(() => {
+    main();
+  }, []);
   return (
-    <div className="App">
-      <canvas id="multiframe">
+    <div className="App" style={{ width: "100%", height: "100%" }}>
+      <canvas id="my-super-id" width="500" height="500">
         Your browser does <strong>not support</strong> the <code>&lt;canvas&gt;</code> element.
       </canvas>
     </div>
