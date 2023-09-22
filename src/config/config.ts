de57297df01b97f2db9f6590e25cfd1f0296e630
avernikoz <avernikoz@gmail.com>
2023-09-22 05:23:51 +0300

    add APP_ENVIRONMENT config

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+export const APP_ENVIRONMENT: "production" | "development" = (() => {
+  if (process.env.REACT_APP_ENVIRONMENT === "production") {
+    return "production";
+  } else {
+    return "development";
+  }
+})();
