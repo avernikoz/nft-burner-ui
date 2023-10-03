23065a32c2c5d2a901fa1fe88e12fbe0d8b2b7dd
avernikoz <avernikoz@gmail.com>
2023-10-03 23:06:49 +0300

    update: - App layout implement: - SolanaWalletContext

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,17 +1,27 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { RenderMain } from "../../webl/script";
+import { SolanaWalletContext } from "../../wallet/solana/SolanaWalletContext";
+import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 function App() {
     useEffect(() => {
         RenderMain();
     }, []);
     return (
-        <div className="App">
-            <canvas id="demo-canvas">
-                Your browser does <strong>not support</strong> the <code>&lt;canvas&gt;</code> element.
-            </canvas>
-        </div>
+        <SolanaWalletContext>
+            <div className="App">
+                <div className="WalletConnectionHeader">
+                    <WalletMultiButton />
+                    <WalletDisconnectButton />
+                </div>
+                <div>
+                    <canvas id="demo-canvas">
+                        Your browser does <strong>not support</strong> the <code>&lt;canvas&gt;</code> element.
+                    </canvas>
+                </div>
+            </div>
+        </SolanaWalletContext>
     );
 }
 
---
+++ src/wallet/solana/SolanaWalletContext.tsx
@@ -0,0 +1,60 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+    Coin98WalletAdapter,
+    CoinbaseWalletAdapter,
+    LedgerWalletAdapter,
+    PhantomWalletAdapter,
+    SolflareWalletAdapter,
+    TrustWalletAdapter,
+    UnsafeBurnerWalletAdapter,
+    WalletConnectWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import React, { FC, PropsWithChildren, useMemo } from "react";
+
+// Default styles that can be overridden by your app
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+export const SolanaWalletContext: FC<PropsWithChildren> = (props) => {
+    const { children } = props;
+    const network = WalletAdapterNetwork.Mainnet;
+    // TODO ASAP IMPORTANT: Add custom RPC
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const wallets = useMemo(
+        () => [
+            /**
+             * Wallets that implement either of these standards will be available automatically.
+             *
+             *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
+             *     (https://github.com/solana-mobile/mobile-wallet-adapter)
+             *   - Solana Wallet Standard
+             *     (https://github.com/solana-labs/wallet-standard)
+             *
+             * If you wish to support a wallet that supports neither of those standards,
+             * instantiate its legacy wallet adapter here. Common legacy adapters can be found
+             * in the npm package `@solana/wallet-adapter-wallets`.
+             */
+            new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
+            new LedgerWalletAdapter(),
+            new WalletConnectWalletAdapter({ network, options: {} }),
+            new CoinbaseWalletAdapter(),
+            new TrustWalletAdapter(),
+            new Coin98WalletAdapter(),
+            new UnsafeBurnerWalletAdapter(),
+        ],
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [network],
+    );
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
