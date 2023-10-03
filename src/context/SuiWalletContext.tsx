443a3e4d1be43f2e84ae27463021f73ccd4065d0
avernikoz <avernikoz@gmail.com>
2023-10-04 01:32:09 +0300

    update: - move all wallet providers to context folder remove: - remove wallets folder add: - SuiWalletContext

---
+++ src/context/SuiWalletContext.tsx
@@ -0,0 +1,15 @@
+import React, { FC, PropsWithChildren } from "react";
+import { WalletProvider, SuiMainnetChain } from "@suiet/wallet-kit";
+
+// Default styles that can be overridden by your app
+require("@suiet/wallet-kit/style.css");
+
+export const SuiWalletContext: FC<PropsWithChildren> = (props) => {
+    const { children } = props;
+
+    return (
+        <WalletProvider autoConnect chains={[SuiMainnetChain]}>
+            {children}
+        </WalletProvider>
+    );
+};
