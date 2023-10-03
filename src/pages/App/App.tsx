ef149e39f4e95d80582f70363c88c1ba9458dead
avernikoz <avernikoz@gmail.com>
2023-10-04 01:32:46 +0300

    add SuiWalletContext to App

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { RenderMain } from "../../webl/script";
-import { SolanaWalletContext } from "../../wallet/solana/SolanaWalletContext";
+import { SolanaWalletContext } from "../../context/SolanaWalletContext";
+import { SuiWalletContext } from "../../context/SuiWalletContext";
 import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { ConnectButton } from "@suiet/wallet-kit";
 
 function App() {
     useEffect(() => {
@@ -10,17 +12,20 @@ function App() {
     }, []);
     return (
         <SolanaWalletContext>
-            <div className="App">
-                <div className="WalletConnectionHeader">
-                    <WalletMultiButton />
-                    <WalletDisconnectButton />
+            <SuiWalletContext>
+                <div className="App">
+                    <div className="WalletConnectionHeader">
+                        <WalletMultiButton />
+                        <WalletDisconnectButton />
+                        <ConnectButton />
+                    </div>
+                    <div>
+                        <canvas id="demo-canvas">
+                            Your browser does <strong>not support</strong> the <code>&lt;canvas&gt;</code> element.
+                        </canvas>
+                    </div>
                 </div>
-                <div>
-                    <canvas id="demo-canvas">
-                        Your browser does <strong>not support</strong> the <code>&lt;canvas&gt;</code> element.
-                    </canvas>
-                </div>
-            </div>
+            </SuiWalletContext>
         </SolanaWalletContext>
     );
 }
