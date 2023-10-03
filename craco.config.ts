a6b761f065ee09fbd5a4f8b797d8bfac0bfa18b0
avernikoz <avernikoz@gmail.com>
2023-10-03 23:06:15 +0300

    update craco config in order to resolve dependencies issues

--- craco.config.ts
+++ craco.config.ts
@@ -1,1 +1,38 @@
-export {};
+// eslint-disable-next-line import/no-extraneous-dependencies
+import webpack from "webpack";
+
+export default {
+    webpack: {
+        configure: {
+            resolve: {
+                fallback: {
+                    zlib: false,
+                    url: false,
+                    https: false,
+                    http: false,
+                    stream: false,
+                    crypto: false,
+                },
+            },
+            ignoreWarnings: [
+                function ignoreSourcemapsloaderWarnings(warning: {
+                    module: { resource: string | string[] };
+                    details: string | string[];
+                }) {
+                    return (
+                        warning.module &&
+                        warning.module.resource.includes("node_modules") &&
+                        warning.details &&
+                        warning.details.includes("source-map-loader")
+                    );
+                },
+            ],
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                Buffer: ["buffer", "Buffer"],
+                process: "process/browser.js",
+            }),
+        ],
+    },
+};
