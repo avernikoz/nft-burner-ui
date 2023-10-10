b42216761d9c7401790f02b5f429bd4abdaf743f
avernikoz <avernikoz@gmail.com>
2023-10-10 21:24:42 +0300

    add RainbowConnectButton to App.tsx

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -5,6 +5,8 @@ import { SolanaWalletContext } from "../../context/SolanaWalletContext";
 import { SuiWalletContext } from "../../context/SuiWalletContext";
 import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { ConnectButton } from "@suiet/wallet-kit";
+import { ConnectButton as RainbowConnectButton } from "@rainbow-me/rainbowkit";
+import { EVMWalletContext } from "../../context/EVMWalletContext";
 
 function App() {
     useEffect(() => {
@@ -13,18 +15,21 @@ function App() {
     return (
         <SolanaWalletContext>
             <SuiWalletContext>
-                <div className="App">
-                    <div className="WalletConnectionHeader">
-                        <WalletMultiButton />
-                        <WalletDisconnectButton />
-                        <ConnectButton />
+                <EVMWalletContext>
+                    <div className="App">
+                        <div className="WalletConnectionHeader">
+                            <WalletMultiButton />
+                            <WalletDisconnectButton />
+                            <ConnectButton />
+                            <RainbowConnectButton />
+                        </div>
+                        <div>
+                            <canvas id="demo-canvas">
+                                Your browser does <strong>not support</strong> the <code>&lt;canvas&gt;</code> element.
+                            </canvas>
+                        </div>
                     </div>
-                    <div>
-                        <canvas id="demo-canvas">
-                            Your browser does <strong>not support</strong> the <code>&lt;canvas&gt;</code> element.
-                        </canvas>
-                    </div>
-                </div>
+                </EVMWalletContext>
             </SuiWalletContext>
         </SolanaWalletContext>
     );
