85b2b0318e0543006996532345cb9aad1e47294b
Evgeny <pol96956@gmail.com>
2023-10-30 23:19:51 +0300

    return old  wallet logic from this branch

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import "./App.css";
-import { RenderMain } from "../../webl/script";
-// import { SolanaWalletContext } from "../../context/SolanaWalletContext";
-// import { SuiWalletContext } from "../../context/SuiWalletContext";
-// import { EVMWalletContext } from "../../context/EVMWalletContext";
-import { Footer } from "./app.styled";
-import FullScreenButton from "../../components/fullscreen-button/FullscreenButton";
-import Wallets from "../../components/wallets/Wallets";
+import './App.css';
+import { RenderMain } from '../../webl/script';
+import { SolanaWalletContext } from '../../context/SolanaWalletContext';
+import { SuiWalletContext } from '../../context/SuiWalletContext';
+import { EVMWalletContext } from '../../context/EVMWalletContext';
+import { Footer } from './app.styled';
+import FullScreenButton from '../../components/fullscreen-button/FullscreenButton';
+import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { ConnectButton } from '@suiet/wallet-kit';
+import { ConnectButton as RainbowConnectButton } from '@rainbow-me/rainbowkit';
 
 function App() {
     useEffect(() => {
@@ -15,25 +17,29 @@ function App() {
     }, []);
 
     return (
-        // <SolanaWalletContext>
-        //     <SuiWalletContext>
-        //         <EVMWalletContext>
-        <div className="App">
-            <div className="WalletConnectionHeader">
-                <Wallets></Wallets>
-            </div>
-            <div>
-                <canvas id="demo-canvas">
-                    Your browser does <strong>not support</strong> the <code>&lt;canvas&gt;</code> element.
-                </canvas>
-            </div>
-            <Footer>
-                <FullScreenButton />
-            </Footer>
-        </div>
-        //         </EVMWalletContext>
-        //     </SuiWalletContext>
-        // </SolanaWalletContext>
+        <SolanaWalletContext>
+            <SuiWalletContext>
+                <EVMWalletContext>
+                    <div className="App">
+                        <div className="WalletConnectionHeader">
+                            <WalletMultiButton/>
+                            <WalletDisconnectButton/>
+                            <ConnectButton/>
+                            <RainbowConnectButton/>
+                        </div>
+                        <div>
+                            <canvas id="demo-canvas">
+                                Your browser does <strong>not
+                                support</strong> the <code>&lt;canvas&gt;</code> element.
+                            </canvas>
+                        </div>
+                        <Footer>
+                            <FullScreenButton/>
+                        </Footer>
+                    </div>
+                </EVMWalletContext>
+            </SuiWalletContext>
+        </SolanaWalletContext>
     );
 }
 
