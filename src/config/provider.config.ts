ef2d0483ad4a9e5baef1f002527e539c72fa76ef
Evgeny <pol96956@gmail.com>
2023-11-16 22:51:57 +0300

    change solana connection in list

--- src/config/provider.config.ts
+++ src/config/provider.config.ts
@@ -2,5 +2,5 @@
 import { SuiClient } from "@mysten/sui.js/client";
 import { Connection } from "@solana/web3.js";
 
-export const suiProvider = new SuiClient({ url: "https://mainnet.sui.rpcpool.com" });
+export const suiProvider = new SuiClient({ url: "https://sui-mainnet-rpc.allthatnode.com" });
 export const solanaProvider = new Connection("https://solana-mainnet.rpc.extrnode.com");
