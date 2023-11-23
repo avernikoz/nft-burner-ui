e87095795581b17e3b7ff5ecceec49a612296a4b
avernikoz <avernikoz@gmail.com>
2023-11-23 22:03:06 +0300

    add netlify proxy

--- netlify/functions/cors-proxy/cors-proxy.ts
+++ netlify/functions/cors-proxy/cors-proxy.ts
@@ -1,12 +1,38 @@
 import { Handler } from "@netlify/functions";
+import fetch from "node-fetch";
+
+// TODO: Add CORS for the CORS proxy
 
 export const handler: Handler = async (event, context) => {
-    const { name = "stranger" } = event.queryStringParameters;
+    const rawUrl = event.rawUrl;
+    const url = rawUrl.split("/.netlify/functions/cors-proxy/")[1];
+
+    if (!url) {
+        return {
+            statusCode: 400,
+            body: "Missing URL",
+        };
+    }
+
+    const response = await fetch(new URL(url));
+    const buffer = await response.buffer();
+    const bufferBase64 = buffer.toString("base64");
+    const contentType = response.headers.get("content-type");
+
+    if (!contentType) {
+        return {
+            statusCode: 500,
+            body: "Missing Content-Type in response headers",
+        };
+    }
 
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            message: `Hello, ${name}!`,
-        }),
+        isBase64Encoded: true,
+        body: bufferBase64,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": contentType,
+        },
     };
 };
