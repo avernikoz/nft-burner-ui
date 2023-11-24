9be60cc1d37414f0b84c6918319ff8c776a8911d
Evgeny <pol96956@gmail.com>
2023-11-24 18:37:39 +0300

    change rpc

--- src/config/provider.config.ts
+++ src/config/provider.config.ts
@@ -2,5 +2,7 @@
 import { SuiClient } from "@mysten/sui.js/client";
 import { Connection } from "@solana/web3.js";
 
-export const suiProvider = new SuiClient({ url: "https://sui-mainnet-rpc.allthatnode.com" });
+export const suiProvider = new SuiClient({
+    url: "https://mainnet.helius-rpc.com/?api-key=4446ea08-ee75-433b-b078-9919068079ef",
+});
 export const solanaProvider = new Connection("https://solana-mainnet.rpc.extrnode.com");
