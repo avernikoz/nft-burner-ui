f89e2ec68c737ff661cc58a8f89d499e5c4af736
avernikoz <avernikoz@gmail.com>
2023-12-04 17:03:19 +0300

    integrate sentry to app

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,4 +8,6 @@ export const APP_ENVIRONMENT: "production" | "development" = (() => {
     }
 })();
 
+export const sentryDSN = "https://3ca8da718e941b46a893219597159059@o4506315210358784.ingest.sentry.io/4506315213176832";
+
 export const IMAGE_STORE_SINGLETON_INSTANCE = ImageStoreSingleton.getInstance();
