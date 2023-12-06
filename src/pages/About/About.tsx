f59c2d77502479e2599a978334e70cc4b1e1c9f3
avernikoz <avernikoz@gmail.com>
2023-12-06 21:19:35 +0300

    modify about page --- add sections & components

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { RefObject, useRef } from "react";
 import "./About.css";
 import { ERenderingState, GRenderingStateMachine } from "../../webl/states";
 import { ProgressBar } from "./ProgressBar";
 
-export const About = ({ setAboutPageActive }: { setAboutPageActive: (isAboutPageActive: boolean) => void }) => {
+const sectionTextList = [
+    "The NFT ecosystem is currently saturated with an overwhelming number of low-value tokens, diluting the essence of true artistic and collectible value. Burning NFTs serves as a means to clear the clutter, allowing more visibility and recognition for high-quality, meaningful digital creations.",
+    "As the market floods with NFTs lacking genuine artistic or cultural significance, burning tokens becomes a symbolic act of reclaiming authenticity. By letting go of uninspiring assets, users contribute to a narrative that champions quality over quantity, renewing the focus on genuine artistic expression.",
+    "The low volume and diminished value of the NFT market have led some to question the very foundation on which it stands. Burning NFTs invites a conversation about redefining value within the digital art space. It prompts us to explore alternative models that prioritize the intrinsic worth of the creations over speculative trading.",
+    "In this subtle dance between destruction and creation, those who embark on the journey of burning NFTs are not seeking mere attention; rather, they are embracing a role as contributors to a larger narrative of reinvention. It's a nuanced approach, allowing for personal expression within the evolving dynamics of the digital art space, where visibility is earned through meaningful performances.",
+];
+
+export const AboutFirstSection = ({
+    setAboutPageActive,
+    setShowMore,
+}: {
+    setAboutPageActive: (isAboutPageActive: boolean) => void;
+    setShowMore: () => void;
+}) => {
     return (
-        <>
+        <div className="section">
             <ProgressBar />
             <div className="intro_quote">
                 <p>
@@ -24,7 +37,48 @@ export const About = ({ setAboutPageActive }: { setAboutPageActive: (isAboutPage
             >
                 START
             </button>
-            <button className="aboutButton">ABOUT</button>
-        </>
+            <button className="aboutButton" onClick={() => setShowMore()}>
+                ABOUT
+            </button>
+        </div>
+    );
+};
+
+export const AboutSecondSection = ({ refProp }: { refProp: RefObject<HTMLDivElement> }) => {
+    return (
+        <div className="section" ref={refProp}>
+            <div className="intro_quote">
+                <p>
+                    The proposal on the table is unconventional yet thought-provoking: the deliberate act of burning
+                    NFTs. This concept, borne out of a desire to revitalize a market drowning in mediocrity, seeks to
+                    spark a renaissance by shedding the weight of underwhelming digital assets.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export const AboutGenericSection = ({ sectionText }: { sectionText: string }) => {
+    return (
+        <div className="section">
+            <div className="intro_quote">
+                <p>{sectionText}</p>
+            </div>
+        </div>
+    );
+};
+
+export const About = ({ setAboutPageActive }: { setAboutPageActive: (isAboutPageActive: boolean) => void }) => {
+    const myRef: RefObject<HTMLDivElement> = useRef(null);
+    const executeScroll = () => myRef.current?.scrollIntoView({ behavior: "smooth" });
+
+    return (
+        <div className="sectionContainer">
+            <AboutFirstSection setAboutPageActive={setAboutPageActive} setShowMore={executeScroll} />
+            <AboutSecondSection refProp={myRef} />
+            {sectionTextList.map((text) => (
+                <AboutGenericSection sectionText={text} />
+            ))}
+        </div>
     );
 };
