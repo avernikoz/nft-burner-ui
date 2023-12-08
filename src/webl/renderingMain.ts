603698f651e597c7552bfcfe61b691fa6775fda8
OxOOO1 <54439218+OxOOO1@users.noreply.github.com>
2023-12-08 20:16:24 +0300

    8 blur passes

--- src/webl/renderingMain.ts
+++ src/webl/renderingMain.ts
@@ -177,7 +177,7 @@ const GPostProcessPasses: {
     Bloom: null,
     FlamePostProcess: null,
     Combiner: null,
-    BloomNumBlurPasses: 4,
+    BloomNumBlurPasses: 8,
     RenderTargetMIPForBloom: 4,
 };
 
