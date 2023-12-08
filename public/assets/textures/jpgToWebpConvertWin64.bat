c5cd1ffd025ee51f11222fd9cdd1b5776605d027
OxOOO1 <54439218+OxOOO1@users.noreply.github.com>
2023-12-08 17:16:53 +0300

    use png for normal maps

--- public/assets/textures/jpgToWebpConvertWin64.bat
+++ public/assets/textures/jpgToWebpConvertWin64.bat
@@ -3,6 +3,6 @@ rem Delete existing WebP files
 del *.webp
 
 rem Convert JPEG and PNG to WebP with maximum quality
-for %%i in (*.jpg *.png) do (
+for %%i in (*.jpg) do (
     cwebp "%%i" -q 100 -o "%%~ni.webp"
 )
