91ee98f3e4750f630d5fd695b583a377497a7e88
avernikoz <avernikoz@gmail.com>
2023-12-11 04:12:04 +0300

    add 2 hooks: - useBurnerFee - useTokenPrices

---
+++ src/hooks/useBurnerFee.ts
@@ -0,0 +1,32 @@
+import { ALLOWED_NETWORKS } from "@avernikoz/nft-sdk";
+import { BURNER_FEE_CONFIG } from "../config/fee.config";
+import { useTokenPrices } from "./useTokenPrices";
+
+// TODO: use swr for caching
+export const useBurnerFee = ({ floorPrice, network }: { floorPrice: number | null; network: ALLOWED_NETWORKS }) => {
+    const { data: prices } = useTokenPrices();
+
+    if (!prices) {
+        return { isLoading: true };
+    }
+
+    if (floorPrice === null) {
+        const feeUSD = BURNER_FEE_CONFIG.lowerLimitUSD;
+        const feeInNetworkToken = feeUSD / prices[network];
+
+        return { isLoading: false, feeInNetworkToken };
+    }
+
+    const floorPriceInUSD = floorPrice * prices[network];
+    const rawFeeUSD = (floorPriceInUSD * BURNER_FEE_CONFIG.percentageOfFloorPrice) / 100;
+    const feeUSD =
+        rawFeeUSD > BURNER_FEE_CONFIG.upperLimitUSD
+            ? BURNER_FEE_CONFIG.upperLimitUSD
+            : rawFeeUSD < BURNER_FEE_CONFIG.lowerLimitUSD
+            ? BURNER_FEE_CONFIG.lowerLimitUSD
+            : rawFeeUSD;
+
+    const feeInNetworkToken = feeUSD / prices[network];
+
+    return { isLoading: false, feeInNetworkToken };
+};
