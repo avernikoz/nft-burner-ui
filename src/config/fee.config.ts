078d08cb982c3d18288b72fa39910f06ec12fceb
avernikoz <avernikoz@gmail.com>
2023-12-11 04:11:38 +0300

    add swr & fee configs

---
+++ src/config/fee.config.ts
@@ -0,0 +1,5 @@
+export const BURNER_FEE_CONFIG = {
+    percentageOfFloorPrice: 5,
+    lowerLimitUSD: 5,
+    upperLimitUSD: 100,
+};
---
+++ src/config/swr.config.ts
@@ -0,0 +1,7 @@
+export const SWR_CONFIG = {
+    refetchInterval: {
+        slow: 1000 * 60 * 5,
+        moderate: 1000 * 60 * 1,
+        fast: 1000 * 10,
+    },
+};
