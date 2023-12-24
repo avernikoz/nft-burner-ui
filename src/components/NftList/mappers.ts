f1c49467e61704fd4ba5e5d4f0c515dd62f666f1
avernikoz <avernikoz@gmail.com>
2023-12-24 17:14:33 +0300

    fix type

--- src/components/NftList/mappers.ts
+++ src/components/NftList/mappers.ts
@@ -10,7 +10,7 @@ export function suiMapper(
     nfts: {
         name: string;
         url: string;
-        description: string;
+        description: string | null | undefined;
         kioskId: string;
         nftId: string;
         nftType: string;
