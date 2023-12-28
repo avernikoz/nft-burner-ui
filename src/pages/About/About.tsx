da20568a63421b688738343afb722fe476c9b383
avernikoz <avernikoz@gmail.com>
2023-12-28 16:55:33 +0300

    fix bg about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -913,6 +913,10 @@ export const CallToActionBackground = styled.div`
         linear-gradient(0deg, rgba(11, 11, 12, 0.2) 0%, rgba(11, 11, 12, 0.2) 100%),
         url("../assets/lpAssets/EcoPage.webp"),
         lightgray -91.461px -119.922px / 114.291% 194.021% no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
     display: flex;
     align-items: center;
     justify-content: center;
