52410e8a9c10f3532cb469f940347f9273c1ac56
avernikoz <avernikoz@gmail.com>
2024-02-15 12:56:26 +0300

    update level component to be dynamic

--- src/components/Level/Level.tsx
+++ src/components/Level/Level.tsx
@@ -1,100 +1,79 @@
-import { styled } from "styled-components";
+import { Ref, useRef } from "react";
+import { Circle, CircleContainer, LevelContainer, LevelNumber, LevelText, StyledSvgLevel } from "./Level.styled";
+import { Tooltip } from "primereact/tooltip";
 
-export const MainLevelContainer = styled.div`
-    position: absolute;
-    top: 120%;
-    right: 7%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    gap: 16px;
-    //align-items: center;
-    background: transparent;
-`;
+export const Level = ({
+    level,
+    points,
+    levelSize = 200,
+    showLevelText = false,
+    showTooltip = false,
+}: {
+    level: number;
+    points: number;
+    levelSize?: number;
+    showLevelText: boolean;
+    showTooltip: boolean;
+}) => {
+    const fontSizePercentage = 0.6; // 60%
+    const fontSize = levelSize * fontSizePercentage;
+    const paddingPercentage = 0.05; // 5%
+    const padding = levelSize * paddingPercentage;
+    const strokeWidthPercentage = 0.04; // 4%
+    const strokeWidth = levelSize * strokeWidthPercentage;
+    const radiusPercentage = 0.375; // 37.5%
+    const radius = levelSize * radiusPercentage;
 
-export const LevelText = styled.span`
-    font-size: 1.5rem;
-    font-family: Rubik;
-    font-weight: 500;
-    color: #ffffff;
-    text-shadow: 0 0 45px #25fe9578;
-`;
-
-export const Number = styled.span`
-    font-size: 7.5rem;
-    font-family: Khand;
-    color: #ff6a2d;
-    font-weight: 400;
-    /* position: absolute;
-    left: 50%;
-    top: 50%; */
-    text-shadow:
-        0 0 45px #25fe9592,
-        0 0 15px #ff682d97;
-`;
-
-export const CircleContainer = styled.div`
-    /* position: absolute;
-    top: 19%;
-    right: 7%; */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    /* height: 250px;
-    width: 250px; */
-    background: transparent;
-`;
-
-export const Circle = styled.circle<{ $circumference?: number }>`
-    stroke-dasharray: 0 ${(props) => props.$circumference} 0;
-    transition: stroke-dasharray 0.5s ease-out;
-    transform: rotate(-180deg);
-    transform-origin: 50% 50%;
-`;
-
-export const StyledSvgLevel = styled.svg`
-    position: absolute;
-`;
-
-export const Level = ({ level, points }: { level: number; points: number }) => {
-    const radius = 75;
     const circumference = 2 * Math.PI * radius;
     const offset = (points / 100) * circumference;
 
-    return (
-        <MainLevelContainer>
-            <LevelText>LEVEL</LevelText>
-            <CircleContainer>
-                <StyledSvgLevel width={200} height={200}>
-                    <Circle
-                        stroke="#3f3f3f96"
-                        fill="transparent"
-                        strokeWidth="8"
-                        strokeDasharray={`${circumference} ${circumference}`}
-                        style={{ strokeDashoffset: circumference }}
-                        r={radius}
-                        cx={100}
-                        cy={100}
-                        $circumference={circumference}
-                    />
-                </StyledSvgLevel>
-                <StyledSvgLevel width={200} height={200}>
-                    <Circle
-                        stroke="#ffffff"
-                        fill="transparent"
-                        strokeWidth="8"
-                        strokeDasharray={`${circumference} ${circumference}`}
-                        style={{ strokeDashoffset: -offset, transform: "rotate(-90 0 0)" }}
-                        r={radius}
-                        cx={100}
-                        cy={100}
-                        $circumference={circumference}
-                        filter="drop-shadow(0px 0px 12px #25FE96)"
-                    />
-                </StyledSvgLevel>
+    const levelRef = useRef<HTMLDivElement>(null);
 
-                <Number>{level}</Number>
-            </CircleContainer>
-        </MainLevelContainer>
+    return (
+        <>
+            <LevelContainer>
+                {showLevelText && <LevelText>LEVEL</LevelText>}
+                <CircleContainer $levelSize={levelSize} ref={levelRef}>
+                    <StyledSvgLevel width={levelSize} height={levelSize}>
+                        <Circle
+                            stroke="#3f3f3f96"
+                            fill="transparent"
+                            strokeWidth={strokeWidth}
+                            strokeDasharray={`${circumference} ${circumference}`}
+                            style={{ strokeDashoffset: circumference }}
+                            r={radius}
+                            cx={levelSize / 2}
+                            cy={levelSize / 2}
+                            $circumference={circumference}
+                        />
+                    </StyledSvgLevel>
+                    <StyledSvgLevel width={levelSize} height={levelSize}>
+                        <Circle
+                            stroke="#ffffff"
+                            fill="transparent"
+                            strokeWidth={strokeWidth}
+                            strokeDasharray={`${circumference} ${circumference}`}
+                            style={{ strokeDashoffset: -offset, transform: "rotate(-90 0 0)" }}
+                            r={radius}
+                            cx={levelSize / 2}
+                            cy={levelSize / 2}
+                            $circumference={circumference}
+                            filter="drop-shadow(0px 0px 12px #25FE96)"
+                        />
+                    </StyledSvgLevel>
+
+                    <LevelNumber $fontSize={fontSize} $paddingTop={padding}>
+                        {level}
+                    </LevelNumber>
+                </CircleContainer>
+            </LevelContainer>
+            <Tooltip
+                disabled={!showTooltip}
+                className="tooltip-burner-fee"
+                content="Level: The measure of your NFT burning prowess. Increase your level by burning NFTs and unlocking new achievements. The higher your level, the greater your status in the NFT Burner community. "
+                target={levelRef}
+                position="top"
+            />
+        </>
     );
 };
