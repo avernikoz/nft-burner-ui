591a5967146521ff2b3c7acf655850f85d56cd29
EvgenyYakovtsev <pol96956@gmail.com>
2024-04-02 18:50:37 +0300

    fix pre-sale form

--- src/pages/TermoCatLand/components/PresalePhase/PresalePhase.tsx
+++ src/pages/TermoCatLand/components/PresalePhase/PresalePhase.tsx
@@ -4,6 +4,10 @@ import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
 import { PhaseWarning } from "../PhaseWarning/PhaseWarning";
 import { EPhase } from "../../TermoCatModel";
+import { ChangeEvent, FormEvent, useContext, useState } from "react";
+import { ToastContext } from "../../../../components/ToastProvider/ToastProvider";
+import { encode } from "../../../../utils/encode";
+import { sleep } from "../../../../utils/sleep";
 
 const PresalePhaseWrapper = styled.div`
     width: 45vw;
@@ -73,7 +77,60 @@ const SendButton = styled(CopyButton)`
     display: inline-flex;
 `;
 
+export interface FormData {
+    name: string;
+    email: string;
+    message: string;
+}
+
 export const PresalePhase = () => {
+    const [formData, setFormData] = useState<FormData>({
+        name: "",
+        email: "",
+        message: "",
+    });
+    const toastController = useContext(ToastContext);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            // Your form submission logic here
+            const response = await fetch("/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: encode({
+                    "form-name": "contact",
+                    ...formData,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to submit form: ${response.statusText}`);
+            }
+
+            toastController?.showSuccess("Message sent successfully! Thanks for reaching out.");
+
+            // Optional: Wait for 5 seconds before clearing the form
+            await sleep(5000);
+
+            // Clear the form after submission (optional)
+            setFormData({ name: "", email: "", message: "" });
+        } catch (error) {
+            console.error(error);
+            if (error instanceof Error) {
+                toastController?.showError("Failed to send contact message: " + error.message);
+            } else {
+                toastController?.showError("Failed to send contact message: " + error);
+            }
+        }
+    };
     return (
         <div style={{ marginBottom: "2rem" }} id={EPhase.PRE_SALE}>
             <PresalePhaseWrapper>
@@ -85,18 +142,40 @@ export const PresalePhase = () => {
                         to us on X at @nftburnerapp
                     </p>
                 </HeadPhaseSection>
-                <PresaleForm>
-                    <StyledInput type="text" placeholder="Name" required />
-                    <StyledInput type="email" placeholder="Email" required />
-                    <StyledTextarea placeholder="Write your message for us here" required />
-                    <SendButton
-                        type="submit"
-                        onClick={(e) => {
-                            e.stopPropagation();
-                        }}
-                    >
-                        Send
-                    </SendButton>
+                <PresaleForm
+                    onSubmit={handleSubmit}
+                    name="contact"
+                    method="POST"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                >
+                    <input type="hidden" name="form-name" value="contact" />
+                    <input type="hidden" name="bot-field" />
+                    <StyledInput
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                    <StyledInput
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                    <StyledTextarea
+                        value={formData.message}
+                        onChange={handleChange}
+                        rows={4}
+                        name="message"
+                        placeholder="Write your message for us here"
+                        required
+                    />
+                    <SendButton type="submit">Send</SendButton>
                 </PresaleForm>
             </PresalePhaseWrapper>
             <PhaseWarning
