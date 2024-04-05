fb32522840667c2197e7d7a88ca5b9d6fd10e357
avernikoz <avernikoz@gmail.com>
2024-04-05 19:14:01 +0300

    update burning nfts to support bluemove nfts standard

--- src/transactions/handleSuiTransaction.ts
+++ src/transactions/handleSuiTransaction.ts
@@ -13,12 +13,26 @@ export async function handleSuiTransaction({
     burnerFee: number;
 }) {
     const payRes = await SUI_NFT_CLIENT_INSTANCE.pay({ amount: burnerFee });
-    const burnRes = await SUI_NFT_CLIENT_INSTANCE.burnNFT({
-        nft: nft,
-        transaction: payRes.transaction,
-    });
+
+    let burnRes;
+    // Assuming it's OB standard & nft is in kiosk
+    if ("kioskId" in nft && nft.kioskId !== undefined) {
+        const { kioskId } = nft;
+        burnRes = await SUI_NFT_CLIENT_INSTANCE.burnNFT({
+            nft: { kioskId, nftId: nft.id, nftType: nft.nftType },
+            transaction: payRes.transaction,
+        });
+    } else {
+        // Assuming it's Bluemove standard & nft is not in kiosk
+        burnRes = await SUI_NFT_CLIENT_INSTANCE.burnNonKioskBluemoveNFT({
+            nft: nft,
+            transaction: payRes.transaction,
+        });
+    }
+
     const result = await signAndExecuteTransactionBlock({
         transactionBlock: burnRes.transaction,
+        options: { showEffects: true, showObjectChanges: true, showEvents: true },
     });
 
     const transactionStatus = isTransactionSuccessful(result);
